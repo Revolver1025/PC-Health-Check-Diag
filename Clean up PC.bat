f68309cb157424d28c6688d2316d5ad6e4cf7c64
Revolver1025 <tayler.white4030@gmx.co.uk>
2026-05-17 00:19:24 +0100

    Updated file with correct commands

--- Clean up PC.bat
+++ Clean up PC.bat
@@ -1,5 +1,5 @@
 @Echo Off
-@ Echo ## Running System File Check This check for corruted Files ##
-@Echo sfc /scannow
-##Running Disk Image Checker This compares system version against online version, think it as a further integrity check ##
-@Echo DISM /online /Cleanup-image /restorehealth
+@Echo ## Running System File Check This check for corruted Files ##
+sfc /scannow
+@Echo ## Running Disk Image Checker This compares system version against online version, think it as a further integrity check ##
+DISM /Online /Cleanup-image /RestoreHealth
